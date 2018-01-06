a843d0da5b14ae49391ef539809333c0dc5969cf
Joanna Butkiewicz <joanna.butkiewicz@profil-software.com>
2018-01-06 18:45:38 +0100

    Add mysql connector

--- java/mahout/src/main/java/recommender/MahoutOutput.java
+++ java/mahout/src/main/java/recommender/MahoutOutput.java
@@ -2,11 +2,27 @@ package recommender;
 
 import static spark.Spark.get;
 
+import java.sql.*;
+
 public class MahoutOutput {
 
     public static void main(String[] args) {
         get("/", (req, res) -> {
-            return "hello from sparkjava.com";
+            String response = "";
+            try {
+                Connection conn = DriverManager.getConnection("jdbc:mysql://mysql:3306/ikea_db","admin", "admin1234");
+                Statement stmt = conn.createStatement();
+
+                ResultSet rs = stmt.executeQuery("SELECT value FROM catalog_product_entity_varchar WHERE value_id = 7266");
+                while ( rs.next() ) {
+                    String value = rs.getString("value");
+                    response += value;
+                }
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return response;
         });
     }
 }
